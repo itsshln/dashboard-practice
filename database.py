9998ba971be44e345a652091e8e91af270e67d5f
shln <vasshln2003@gmail.com>
2026-06-05 20:34:40 +1000

    Add files via upload

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 
 engine = create_engine(
     f"postgresql+psycopg2://"
@@ -8,7 +8,12 @@ engine = create_engine(
     f"{st.secrets['DB_HOST']}:"
     f"{st.secrets['DB_PORT']}/"
     f"{st.secrets['DB_NAME']}",
-    connect_args={
-        "sslmode": "require"
-    }
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True,
 )
+
+@event.listens_for(engine, "connect")
+def set_search_path(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("SET search_path TO public")
+    cursor.close()
